8b286e662f9947a2cd28da7a87fc20b4d24b5d3d
leixiangyang <leixiangyang@foxmail.com>
2014-11-20 23:48:44 +0800

    modify

---
+++ src/ZdtbSite.Web/Controllers/OurTeamController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZdtbSite.Web.Controllers
+{
+    public class OurTeamController : Controller
+    {
+        //
+        // GET: /OurTeam/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
