c4eac82cb8ae81751121bc9925c315fddd741531
hehe04 <hehe04@qq.com>
2014-12-14 16:19:27 +0800

    产品推荐

--- src/ZdtbSite.Web/Controllers/HomeController.cs
+++ src/ZdtbSite.Web/Controllers/HomeController.cs
@@ -11,17 +11,20 @@ namespace ZdtbSite.Web.Controllers
     public class HomeController : BaseController
     {
         private readonly IRepository<ProductType> productTypeRepository;
+        private readonly IRepository<Product> productRepository;
         private IUnitOfWork unitOfWork;
 
-        public HomeController(IRepository<ProductType> productTypeRepository, IUnitOfWork unitOfWork)
+        public HomeController(IRepository<ProductType> productTypeRepository, IUnitOfWork unitOfWork, IRepository<Product> productRepository)
         {
             this.productTypeRepository = productTypeRepository;
+            this.productRepository = productRepository;
             this.unitOfWork = unitOfWork;
         }
 
         // GET: Home
         public ActionResult Index()
         {
+            var modelList = GetProductRecommendList(productRepository);
             return View();
         }
     }
