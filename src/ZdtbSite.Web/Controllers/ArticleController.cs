69dc89a3d9a18931913c596ac9e4432ec8753886
hehe04 <hehe04@qq.com>
2014-12-24 01:10:40 +0800

    绑定文章详情页

--- src/ZdtbSite.Web/Controllers/ArticleController.cs
+++ src/ZdtbSite.Web/Controllers/ArticleController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZdtbSite.Core.Infrastructure;
+using ZdtbSite.Model;
 
 namespace ZdtbSite.Web.Controllers
 {
     public class ArticleController : BaseController
     {
+        private IRepository<Product> _productRepository;
+        private IRepository<ContentType> _contentTypeRepository;
+
+        public ArticleController(IRepository<Product> productRepository, IRepository<ContentType> contentTypeRepository)
+        {
+            _productRepository = productRepository;
+            _contentTypeRepository = contentTypeRepository;
+        }
         // GET: Article
         public ActionResult Index(string catelog, string keywords, int pageIndex = 1)
         {
@@ -16,6 +26,8 @@ namespace ZdtbSite.Web.Controllers
 
         public ActionResult Detail(int id)
         {
+            ViewBag.ProductRecommendList = GetProductRecommendList(_productRepository);
+            ViewBag.ContentTypes = _contentTypeRepository.GetAllAsNoTracking();
             return View();
         }
     }
