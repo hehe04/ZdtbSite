dac74c28a9c849b64b038f28d92169e63f1d052d
hehe04 <hehe04@qq.com>
2015-01-05 23:15:52 +0800

    添加文件下载页面

---
+++ src/ZdtbSite.Model/Document.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZdtbSite.Model
+{
+    public class Document
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Path { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string CreateBy { get; set; }
+
+        public Document()
+        {
+            CreateDate = DateTime.Now;
+        }
+    }
+}
