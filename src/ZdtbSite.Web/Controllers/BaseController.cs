962275a9b32376647ab792ae7d026473934dc259
hehe04 <hehe04@qq.com>
2015-02-25 14:39:03 +0800

    添加关键词设置，取消后台企业信息中添加删除功能

--- src/ZdtbSite.Web/Controllers/BaseController.cs
+++ src/ZdtbSite.Web/Controllers/BaseController.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Autofac;
+using Autofac.Core;
 using ZdtbSite.Core.Infrastructure;
+using ZdtbSite.Core.Repository;
 using ZdtbSite.Model;
-using ZdtbSite.Web.Models;
+using ZdtbSite.Web.Areas.ZdtbAdmin.Models;
+using ProductViewModel = ZdtbSite.Web.Models.ProductViewModel;
 
 namespace ZdtbSite.Web.Controllers
 {
@@ -13,6 +17,7 @@ namespace ZdtbSite.Web.Controllers
     {
         protected override void HandleUnknownAction(string actionName)
         {
+            ViewBag.SiteKeywords = GetSiteKeywords();
             View(actionName).ExecuteResult(ControllerContext);
         }
 
@@ -29,8 +34,21 @@ namespace ZdtbSite.Web.Controllers
             {
                 productType = int.Parse(cookie.Value);
             }
-            var list=repository.GetManyAsNoTracking(x => x.ProductTypeId == productType).Take(3);
+            var list = repository.GetManyAsNoTracking(x => x.ProductTypeId == productType).Take(3);
             return AutoMapper.Mapper.Map<IEnumerable<Product>, IEnumerable<ProductViewModel>>(list);
+        }
+
+        public BasicInfoViewModel GetSiteKeywords()
+        {
+            var repository = DependencyResolver.Current.GetService<IRepository<BasicInfo>>();
+            var siteKeywords = repository.GetById(11);
+            return AutoMapper.Mapper.Map<BasicInfo, BasicInfoViewModel>(siteKeywords);
+        }
+
+        protected override void OnActionExecuting(ActionExecutingContext ctx)
+        {
+            base.OnActionExecuting(ctx);
+            ViewBag.SiteKeywords = GetSiteKeywords();
         }
     }
 }
